189ad6e1ab65115dd2141724d72dc95c34b7b9d4
bo_knows <lawrence.boland@gmail.com>
2015-11-13 21:14:36 -0500

    Modularizing javascript files

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 'use strict';
 
-(function(){
-	var app = angular.module('hex', []);
+var MODULE = (function(hex){
+	var app = angular.module('hexGame', []);
 
 	app.controller('PlayerListController', function(){
 		this.players = players;
+		this.rows = hex.rows;
 	});
 
+	console.log(hex.rows);
+	setInterval(function(){ hex.rows = hex.rows+1;}, 3000);
 	var players = [
 		{name: 'Bo'},
 		{name: 'Marlon'}
 	];
-})();
+
+	return hex;
+}(MODULE));
 
